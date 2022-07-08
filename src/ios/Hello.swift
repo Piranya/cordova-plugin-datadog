3b1bc4039ee56beac7db10d6cdb0532efc3847b9
Nick Tkachenko <piranya87@gmail.com>
2022-07-08 17:09:56 +0100

    Update Hello.swift

--- src/ios/Hello.swift
+++ src/ios/Hello.swift
@@ -1,52 +1,17 @@
-@objc(Hello) class Hello : CDVPlugin {
-    func sayHello(command: CDVInvokedUrlCommand) {
-        var pluginResult = CDVPluginResult(
-            status: CDVCommandStatus_ERROR
-        )
-
-        let msg = command.arguments[0] as? String ?? ""
-
-        if msg.characters.count > 0 {
-            /* UIAlertController is iOS 8 or newer only. */
-            let toastController: UIAlertController = 
-                UIAlertController(
-                    title: "", 
-                    message: msg, 
-                    preferredStyle: .Alert
-                )
-
-            self.viewController?.presentViewController(
-                toastController, 
-                animated: true, 
-                completion: nil
-            )
-
-            let duration = Double(NSEC_PER_SEC) * 3.0
-            dispatch_after(
-                dispatch_time(
-                    DISPATCH_TIME_NOW, 
-                    Int64(duration)
-                ), 
-                dispatch_get_main_queue(), 
-                { 
-                    toastController.dismissViewControllerAnimated(
-                        true, 
-                        completion: nil
-                    )
-                }
-            )
-
-            pluginResult = CDVPluginResult(
-                status: CDVCommandStatus_OK,
-                messageAsString: msg
-            )
-        }
-
-        self.commandDelegate!.sendPluginResult(
-            pluginResult, 
-            callbackId: command.callbackId
-        )
-    }
+@objc(Hello) class Hello : CDVPlugin{
+var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+@objc(sayHello:) func sayHello(_ command: CDVInvokedUrlCommand) {
+var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
+let param1 = (command.arguments[0] as? NSObject)?.value(forKey: "param1") as? Int
+let param2 = (command.arguments[0] as? NSObject)?.value(forKey: "param2") as? Int
+if let p1 = param1 , let p2 = param2 {
+if p1 >= 0 && p1 >= 0{
+ let total = String(p1 + p2)
+pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: total)
+}else {
+pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR, messageAs: "Something wrong")
 }
-
-
+}
+self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
+}
+}
