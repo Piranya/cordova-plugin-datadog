d6ece49b219ea92fa60b4e31e9faca16994283be
Nick Tkachenko <piranya87@gmail.com>
2022-07-08 15:52:59 +0100

    Update Hello.swift

--- src/ios/Hello.swift
+++ src/ios/Hello.swift
@@ -5,9 +5,12 @@ import Foundation
 
 @objc(Hello) class DemoPlugin : CDVPlugin{
     var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-    @objc(sayHello:) func sayHello(_ command: CDVInvokedUrlCommand) {
-      pluginResult = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: command.arguments[0])
+    @objc(sayHello:) func sayHello(command: CDVInvokedUrlCommand) {
+        self.commandCallback = command.callbackId
+        let input = command.arguments.first as! String
+        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: input!)
+
+         self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
     }
-    self.commandDelegate!.send(pluginResult, callbackId: command.callbackId)
 }
 
