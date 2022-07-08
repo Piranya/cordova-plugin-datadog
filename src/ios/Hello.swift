20cba095ba1e98d33bf8293623dc448966603307
Nick Tkachenko <piranya87@gmail.com>
2022-07-08 16:58:07 +0100

    Update Hello.swift

--- src/ios/Hello.swift
+++ src/ios/Hello.swift
@@ -2,15 +2,55 @@ import UIKit
 import MobileCoreServices
 import Foundation
 
+@objc(Hello) class Hello : CDVPlugin {
+    func sayHello(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
 
-@objc(Hello) class Hello : CDVPlugin{
-    var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
-    @objc(sayHello:) func sayHello(command: CDVInvokedUrlCommand) {
-        self.commandCallback = command.callbackId
-        let input = command.arguments.first as! String
-        pluginResult = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: input!)
+        let msg = command.arguments[0] as? String ?? ""
 
-         self.commandDelegate.send(pluginResult, callbackId: command.callbackId)
+        if msg.characters.count > 0 {
+            /* UIAlertController is iOS 8 or newer only. */
+            let toastController: UIAlertController = 
+                UIAlertController(
+                    title: "", 
+                    message: msg, 
+                    preferredStyle: .Alert
+                )
+
+            self.viewController?.presentViewController(
+                toastController, 
+                animated: true, 
+                completion: nil
+            )
+
+            let duration = Double(NSEC_PER_SEC) * 3.0
+            dispatch_after(
+                dispatch_time(
+                    DISPATCH_TIME_NOW, 
+                    Int64(duration)
+                ), 
+                dispatch_get_main_queue(), 
+                { 
+                    toastController.dismissViewControllerAnimated(
+                        true, 
+                        completion: nil
+                    )
+                }
+            )
+
+            pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK,
+                messageAsString: msg
+            )
+        }
+
+        self.commandDelegate!.sendPluginResult(
+            pluginResult, 
+            callbackId: command.callbackId
+        )
     }
 }
+
 
