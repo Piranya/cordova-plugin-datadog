ecdc453fb247cd3e9262738fd902245758fc65ab
Nick Tkachenko <piranya87@gmail.com>
2022-07-18 17:51:32 +0100

    fix duplicate declare

--- src/ios/Datadog.swift
+++ src/ios/Datadog.swift
@@ -73,14 +73,7 @@ import DatadogCrashReporting
         self.commandDelegate!.send(result, callbackId: command.callbackId)
     }
     
-    @objc(setUserName:)func setUserName(command : CDVInvokedUrlCommand){
-         var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
-         let userName = command.argument(at: 0) as! String
-       //  Datadog.setUserInfo(name: userName)
-         result = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate!.send(result, callbackId: command.callbackId)
-    }
-    
+
     @objc(getSessionId:)func getSessionId(command : CDVInvokedUrlCommand){
          var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
         let uuid = UUID().uuidString
