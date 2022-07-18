9e69d3f8d8d7982eb008adae8257e73cecf25e1f
Nick Tkachenko <piranya87@gmail.com>
2022-07-18 16:29:05 +0100

    unset userinfo

--- src/ios/Datadog.swift
+++ src/ios/Datadog.swift
@@ -83,7 +83,7 @@ import DatadogCrashReporting
     @objc(setUserName:)func setUserName(command : CDVInvokedUrlCommand){
          var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
          let userName = command.argument(at: 0) as! String
-         Datadog.setUserInfo(name: userName)
+       //  Datadog.setUserInfo(name: userName)
          result = CDVPluginResult(status: CDVCommandStatus_OK)
         self.commandDelegate!.send(result, callbackId: command.callbackId)
     }
