cb5f1634b7263b8adf39e876d33b8ea50e21251d
Nick Tkachenko <piranya87@gmail.com>
2024-06-26 16:47:39 +0100

    working test version
    working test version. session is visible in datadog on sub clone application

--- src/ios/Datadog.swift
+++ src/ios/Datadog.swift
@@ -1,67 +1,18 @@
 import Foundation
-import Datadog
+import DatadogCore
+import DatadogSessionReplay
+import DatadogRUM
 import DatadogCrashReporting
+import DatadogLogs
 
 @objc(Datadog) class DatadogCrash: CDVPlugin{
     
-    var wkSessionId:String = " "
+var wkSessionId:String = " "
     var isInitialized:Bool = false
     var pluginResult = CDVPluginResult(status: CDVCommandStatus_ERROR)
     
     @objc(Init:)func Init(command : CDVInvokedUrlCommand){
-         var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
-        if(!isInitialized){
-            let clientToken = command.argument(at: 0) as! String
-            let environment = command.argument(at: 1) as! String
-            let appID = command.argument(at: 2) as! String
-            let trackingConsentInt = command.argument(at: 3) as! Int
-            let serviceName = command.argument(at: 4) as! String
-            var trackingConsent:TrackingConsent
-            switch trackingConsentInt {
-            case 0:
-                trackingConsent = .notGranted
-                break;
-            case 1:
-                trackingConsent = .granted
-                break;
-            default:
-                trackingConsent = .pending
-                break;
-            }
-            Datadog.initialize(
-                appContext: .init(),
-                trackingConsent: trackingConsent,
-                configuration: Datadog.Configuration
-                .builderUsing(
-                    rumApplicationID: appID,
-                    clientToken: clientToken,
-                    environment: environment
-                )        
-                .set(serviceName: serviceName)
-                .trackUIKitRUMViews()
-                .trackUIKitRUMActions()
-                .trackURLSession()
-                .enableCrashReporting(using: DDCrashReportingPlugin())
-                .build()
-            )
-            Datadog.verbosityLevel = .debug
-            Global.rum = RUMMonitor.initialize()
-
-            
-            if self.wkSessionId.compare(" ") != .orderedSame {
-                Global.rum.addAttribute(forKey: "wk_UniqueIDForSession", value: wkSessionId)
-            }
-          
-          
-            
-            //let result = CDVPluginResult.init(status: CDVCommandStatus_OK)
-             result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Initialized!")
-            self.commandDelegate!.send(result, callbackId: command.callbackId)
-        }else{
-            //let result = CDVPluginResult.init(status: CDVCommandStatus_OK, messageAs: "Already Initialized!")
-             result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Already Initialized!")
-            self.commandDelegate!.send(result, callbackId: command.callbackId)
-        }
+       
     }
     
     @objc(InitTrack:)func InitTrack(command : CDVInvokedUrlCommand){
@@ -85,34 +36,44 @@ import DatadogCrashReporting
                 break;
             }
             
-            let session = URLSession(configuration: .default, delegate: DDURLSessionDelegate(), delegateQueue: nil)
+let session = URLSession(configuration: .default, delegate: DDURLSessionDelegate(), delegateQueue: nil)
 
-            Datadog.initialize(
-                appContext: .init(),
-                trackingConsent: trackingConsent,
-                configuration: Datadog.Configuration
-                .builderUsing(
-                    rumApplicationID: appID,
-                    clientToken: clientToken,
-                    environment: environment
-                )        
-                .set(serviceName: serviceName)
-                .trackUIKitRUMViews()
-                .trackUIKitRUMActions()
-                .trackURLSession()
-                .trackBackgroundEvents()
-                .enableCrashReporting(using: DDCrashReportingPlugin())
-                .build()
-            )
-            Datadog.verbosityLevel = .debug
-            Global.rum = RUMMonitor.initialize()
 
             
-            if self.wkSessionId.compare(" ") != .orderedSame {
-                Global.rum.addAttribute(forKey: "wk_UniqueIDForSession", value: wkSessionId)
-            }
-          
-          
+Datadog.initialize(
+  with: Datadog.Configuration(
+    clientToken: clientToken,
+    env: environment,
+    service: serviceName
+  ),
+  trackingConsent: trackingConsent
+)
+
+ SessionReplay.enable(
+       with: SessionReplay.Configuration(
+           replaySampleRate: 50
+       )
+   )
+Logs.enable()
+   
+Datadog.verbosityLevel = .debug
+
+   let logger = Logger.create(
+	with: Logger.Configuration(
+		name: "<Test logger name>",
+		networkInfoEnabled: true,
+		remoteLogThreshold: .info,
+		consoleLogFormat: .shortWith(prefix: "[iOS App] ")
+	)
+    )
+
+// logger.info("Test message")
+// logger.debug("A debug message.")
+// logger.info("Some relevant information?")
+// logger.notice("Have you noticed?")
+// logger.warn("An important warning…")
+// logger.error("An error was met!")
+// logger.critical("Something critical happened!")
             
             //let result = CDVPluginResult.init(status: CDVCommandStatus_OK)
              result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: "Initialized!")
@@ -126,95 +87,59 @@ import DatadogCrashReporting
     
     
     @objc(setCustomFieldSessionId:)func setCustomFieldSessionId(command : CDVInvokedUrlCommand){
-         var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
-        wkSessionId = command.argument(at: 0) as! String
-        if isInitialized {
-            if self.wkSessionId.compare(" ") != .orderedSame {
-                Global.rum.removeAttribute(forKey: "wk_UniqueIDForSession")
-            }
-            Global.rum.addAttribute(forKey: "wk_UniqueIDForSession", value: wkSessionId)
-        }
-         result = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate!.send(result, callbackId: command.callbackId)
-    }
+           }
     
 
     @objc(getSessionId:)func getSessionId(command : CDVInvokedUrlCommand){
-         var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
-        let uuid = UUID().uuidString
-         result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: uuid)
-        self.commandDelegate!.send(result, callbackId: command.callbackId)
+      
     }
     
     @objc(loggerMessage:)func loggerMessage(command : CDVInvokedUrlCommand){
-         var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
         
-         let logType = command.argument(at: 0) as! String
-         let message = command.argument(at: 1) as! String
-        
-            let logger = Logger.builder
-            .sendNetworkInfo(true)
-            .printLogsToConsole(true, usingFormat: .shortWith(prefix: "[iOS App] "))
-            .build()
-        
-            switch logType {
-            case "debug":
-                logger.debug(message)
-                break;
-            case "info":
-                logger.info(message)
-                break;
-            case "notice":
-                logger.notice(message)
-                break;
-             case "warn":
-                logger.warn(message)
-                break;
-             case "error":
-                logger.error(message)
-                break;
-             case "critical":
-                logger.critical(message)
-                break;
-            default:
-                logger.debug("default log message")
-                break;
-            }
-            
-            
-        
-         result = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate!.send(result, callbackId: command.callbackId)
     }
 
     @objc(setTrackingConsent:)func setTrackingConsent(command : CDVInvokedUrlCommand){
-         var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
-        let trackingConsentInt = command.argument(at: 0) as! Int
-        var trackingConsent:TrackingConsent
-        switch trackingConsentInt {
-        case 0:
-            trackingConsent = .notGranted
-            break;
-        case 1:
-            trackingConsent = .granted
-            break;
-        default:
-            trackingConsent = .pending
-            break;
-        }
-        Datadog.set(trackingConsent: trackingConsent)
-         result = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate!.send(result, callbackId: command.callbackId)
+      
     }
     
     
     @objc(setUserName:)func setUserName(command : CDVInvokedUrlCommand){
-         var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
-         let userName = command.argument(at: 0) as! String
-         Datadog.setUserInfo(name: userName)
-         result = CDVPluginResult(status: CDVCommandStatus_OK)
-        self.commandDelegate!.send(result, callbackId: command.callbackId)
+        
     }
+    
+    
+    // @objc(setCustomFieldSessionId:)func setCustomFieldSessionId(command : CDVInvokedUrlCommand){
+    //      var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    //     wkSessionId = command.argument(at: 0) as! String
+    //     if isInitialized {
+    //         if self.wkSessionId.compare(" ") != .orderedSame {
+    //             Global.rum.removeAttribute(forKey: "wk_UniqueIDForSession")
+    //         }
+    //         Global.rum.addAttribute(forKey: "wk_UniqueIDForSession", value: wkSessionId)
+    //     }
+    //      result = CDVPluginResult(status: CDVCommandStatus_OK)
+    //     self.commandDelegate!.send(result, callbackId: command.callbackId)
+    // }
+    
+
+    // @objc(getSessionId:)func getSessionId(command : CDVInvokedUrlCommand){
+    //      var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    //     let uuid = UUID().uuidString
+    //      result = CDVPluginResult(status: CDVCommandStatus_OK, messageAs: uuid)
+    //     self.commandDelegate!.send(result, callbackId: command.callbackId)
+    // }
+    
+    
+
+  
+    
+    // @objc(setUserName:)func setUserName(command : CDVInvokedUrlCommand){
+    //      var result = CDVPluginResult(status: CDVCommandStatus_ERROR)
+    //      let userName = command.argument(at: 0) as! String
+    //      Datadog.setUserInfo(name: userName)
+    //      result = CDVPluginResult(status: CDVCommandStatus_OK)
+    //     self.commandDelegate!.send(result, callbackId: command.callbackId)
+    // }
     
 
 }
--- www/hello.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    sayHello: function (inputParams, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Hello", "sayHello", [inputParams]);
-    }
-};
